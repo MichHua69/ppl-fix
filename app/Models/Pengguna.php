10b4031fbd45876390da17bb366ebb86d69c0430
Divo Tahta Imannulloh <root@vmi1161549.contaboserver.net>
2024-05-23 01:00:54 +0700

    Production Fix

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Pengguna;
+use App\Models\role;
+use App\Models\dokterhewan;
+use App\Models\peternak;
 
 class Pengguna extends Authenticatable
 {
