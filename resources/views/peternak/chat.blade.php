aed827b0586baf9dff940fd1dcf40d79b870e2cb
MichHua69 <rizalbagushidayatt@gmail.com>
2024-05-25 17:50:07 +0700

    revisi search bar tapi js nya search bar blm

--- resources/views/peternak/chat.blade.php
+++ resources/views/peternak/chat.blade.php
@@ -54,7 +54,7 @@
                                 <div class="relative w-full">
                                     <input type="search" id="search-dropdown"
                                         class="block p-2.5 w-full z-20 text-md text-gray-900 bg-gray-50 rounded-e-lg border-gray-300 border-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                                        placeholder="Cari Puskeswan" required />
+                                        placeholder="Cari Dokter" required />
                                     <button type="submit"
                                         class="absolute top-0 right-0 p-2.5 text-sm font-medium border-gray-300 h-full text-white bg-primary rounded-e-lg border hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
